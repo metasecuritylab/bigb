ce21f964058215630bcf63da77928a733a821063
root <root@HELLOWON-HOME.localdomain>
2021-08-24 13:49:40 +0900

    review issue for jira

--- libJira.py
+++ libJira.py
@@ -11,3 +11,13 @@ PASS = libConfig.GetConfig('JIRA', 'PASS')
 PROJ = libConfig.GetConfig('JIRA', 'PROJECT')
 
 jira = JIRA(server=JIRA_URL, basic_auth=(USER, PASS))
+
+def ReviewIssue(Key):
+    try:
+        issue = jira.issue(Key)
+        summary = issue.fields.summary
+    except JIRAError as e:
+        #print(e.status_code, e.text)
+        return False
+
+    return True
