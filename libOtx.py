79f9ff7a1405016f8e66cb5fb0a4b9d4258b32a0
root <root@HELLOWON-HOME.localdomain>
2021-08-24 13:57:34 +0900

    getting value of lookup response

--- libOtx.py
+++ libOtx.py
@@ -11,3 +11,20 @@ import json
 API_KEY = libConfig.GetConfig('OTX', 'API_KEY')
 URL = libConfig.GetConfig('OTX', 'URL')
 OTX = OTXv2(API_KEY, server=URL)
+
+def getValue(results, keys):
+    if type(keys) is list and len(keys) > 0:
+
+        if type(results) is dict:
+            key = keys.pop(0)
+            if key in results:
+                return getValue(results[key], keys)
+            else:
+                return None
+        else:
+            if type(results) is list and len(results) > 0:
+                return getValue(results[0], keys)
+            else:
+                return results
+    else:
+        return results
