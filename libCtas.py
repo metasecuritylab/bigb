5968b12c7c551615a5abefded3933f3bfcad846d
root <root@HELLOWON-HOME.localdomain>
2021-08-25 12:22:38 +0900

    parse reponse data from CTAS server

--- libCtas.py
+++ libCtas.py
@@ -18,3 +18,42 @@ def GetClassType(data):
             ClassType.append(item['classtype'])
 
     return list(set(ClassType))
+    
+def ParseData(data, address):
+    outlist = []
+    out = {'rules': 'none', 'classtype': 'none', 'raw': 'none'}
+    signature = 'none'
+    proto = 'none'
+    attacker_ip = 'none'
+    attacker_port = 'none'
+    class_type = 'none'
+
+    if 'res_data_attacker' in data:
+        if len(data['res_data_attacker']) < 1:
+            return outlist
+        else:
+            for item in data['res_data_attacker']:
+                if 'signature' in item:
+                    signature = item['signature']
+
+                if 'proto' in item:
+                    proto = item['proto']
+
+                if 'attacker_ip' in item:
+                    attacker_ip = item['attacker_ip']
+
+                if 'attacker_port' in item:
+                    attacker_port = item['attacker_port']
+
+                if 'classtype' in item:
+                    class_type = item['classtype']
+
+                rules = "alert {} {} any -> any {} (msg: {})".format(proto, attacker_ip, attacker_port, signature)
+                out['raw'] = item
+                out['rules'] = rules
+                out['classtype'] = class_type
+                outlist.append(out)
+
+            return outlist
+
+    return False
