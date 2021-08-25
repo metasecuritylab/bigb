8600f110ab9fbe5dba2dd499a1a01567d44be001
root <root@HELLOWON-HOME.localdomain>
2021-08-25 12:21:54 +0900

    defines class type for response of CTAS

--- libCtas.py
+++ libCtas.py
@@ -7,3 +7,14 @@ import json
 import libConfig
 
 URL = libConfig.GetConfig('CTAS', 'URL')
+
+def GetClassType(data):
+    ClassType = []
+
+    for item in data:
+        if item['classtype'] == None:
+            ClassType.append('none')
+        else:
+            ClassType.append(item['classtype'])
+
+    return list(set(ClassType))
