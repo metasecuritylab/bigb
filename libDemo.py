898d34ce12ee2b2ea9add9e98bca6a2f6984a0ef
root <root@HELLOWON-HOME.localdomain>
2021-09-07 14:14:56 +0900

    get traffic from yml file and unittest added

--- libDemo.py
+++ libDemo.py
@@ -3,9 +3,13 @@
 
 import os
 import random
+import yaml
+import libConfig
+import libUtils
 
 DIR = 'demo'
 FNAME = 'bambenek_banjori.ipset'
+LIMIT = libConfig.GetConfig('DEMO', 'LIMIT')
 
 def ParseRules(fname):
     bucket = {}
@@ -23,15 +27,46 @@ def ParseRules(fname):
 
     return bucket
 
-def GetTraffic(stat_done):
+def GetTrafficFile(num, limit):
     fw_dst_hits = 0
     filename = "{}/{}".format(DIR, FNAME)
     fw_dst_ip = ParseRules(filename)
     if not fw_dst_ip:
         return {}, 0
 
+    for i, k in enumerate(fw_dst_ip.copy()):
+        if i > limit-1:
+            del fw_dst_ip[k]
+
     for i, v in fw_dst_ip.items():
         fw_dst_hits = fw_dst_hits + v
 
-    print(fw_dst_hits)
     return fw_dst_ip, fw_dst_hits
+
+def GetTrafficYML(num):
+    fname = 'demo/data.yml'
+    fw_dst_ip = {}
+    fw_dst_hits = 0
+    with open(fname) as f:
+        ret = yaml.load(f, Loader=yaml.FullLoader)
+
+    del ret[num][int(LIMIT):]
+
+    for i in ret[num]:
+        fw_dst_ip[i] = random.randrange(1000000,9000000)
+
+    for i, v in fw_dst_ip.items():
+        fw_dst_hits = fw_dst_hits + v
+
+    return fw_dst_ip, fw_dst_hits
+
+def UnitTest():
+    fw_dst_ip, fw_dst_hits = GetTrafficYML(1)
+    if len(fw_dst_ip):
+        libUtils.UnitTestPrint(True, 'libDemo', 'GetTrafficYML', len(fw_dst_ip))
+    else:
+        libUtils.UnitTestPrint(False, 'libDemo', 'GetTrafficYML', rlen(fw_dst_ip))    
+    if fw_dst_hits:
+        libUtils.UnitTestPrint(True, 'libDemo', 'GetTrafficYML', fw_dst_hits)
+    else:
+        libUtils.UnitTestPrint(False, 'libDemo', 'GetTrafficYML', fw_dst_hits)
